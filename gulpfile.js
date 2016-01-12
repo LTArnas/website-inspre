890c18092ef003cad279ceb6ba217aa887bec01b
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2016-01-12 03:43:26 +0000

    Finished initial setup.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// Compile all SASS to CSS
+gulp.task('csass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+// Watch
+gulp.task('default', function () {
+    gulp.watch('./sass/**/*.scss', ['csass']);
+});
