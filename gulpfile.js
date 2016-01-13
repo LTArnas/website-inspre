648c8adc24978a563365ca5360e1d1c0cab36224
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2016-01-13 05:13:28 +0000

    Fixed setup for SASS to CSS via Gulp workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 // Compile all SASS to CSS
 gulp.task('csass', function () {
     gulp.src('./sass/**/*.scss')
-        .pipe(sass({precision:8}).on('error', sass.logError))
+        //.pipe(sourcemaps.init())
+        .pipe(sass({ precision: 8 }).on('error', sass.logError))
+        //.pipe(sourcemaps.write('./sourcemaps'))
+        .pipe(gulp.dest('./compiledcss'));
+});
+
+// PostCSS
+gulp.task('postcss', function () {
+    var processors = [
+        autoprefixer({ browsers: ['last 2 version'] })
+    ];
+    console.log('wtf');
+    gulp.src('./compiledcss/*.css')
+        .pipe(postcss(processors))
         .pipe(gulp.dest('./css'));
 });
 
 // Watch
 gulp.task('default', function () {
     gulp.watch('./sass/**/*.scss', ['csass']);
+    gulp.watch('./compiledcss/*.css', ['postcss']);
 });
